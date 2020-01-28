cb0e8bca7da7a79cb958579181e601ec8572d6bd
yenniferTobon <ymtobony@unal.edu.co>
2020-01-28 15:40:10 -0500

    index.py task 4, 5 y 6 with jsonify without pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,19 +2,26 @@
 from api.v1.views import app_views
 import models
 from models.city import City
+from flask import jsonify
 import json
 
 @app_views.route('/api/v1/status')
 def json_status():
-    var = '{\n  "status": "OK"\n}\n'
-    return var
+    var = {"status": "OK"}
+    return jsonify(var)
 
 @app_views.route('/api/v1/stats')
 def endpoint_stats():
-    amenities =  '{\n  "amenities": '+str(models.storage.count("Amenity"))+','
-    cities = '\n  "cities": '+str(models.storage.count("City"))+',' 
-    places = '\n  "places": '+str(models.storage.count("Place"))+','
-    reviews = '\n  "reviews": '+str(models.storage.count("Review"))+','
-    states = '\n  "states": '+str(models.storage.count("State"))+','
-    users = '\n  "users": '+str(models.storage.count("User"))+'\n}\n'
-    return amenities + cities + places + reviews + states + users
+    new_obj = {"amenities" : models.storage.count("Amenity"),
+                "cities" : models.storage.count("City"),
+                "places": models.storage.count("Place"),
+                "reviews": models.storage.count("Review"),
+                "states": models.storage.count("State"),
+                "users": models.storage.count("User")
+    }
+    return jsonify(new_obj)
+
+@app_views.route('/api/v1/nop')
+def errors_404():
+    error = {'error': 'Not found'}
+    return jsonify(error)
