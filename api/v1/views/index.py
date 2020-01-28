dfaaaa5c954503953d86e73d11de775b60a2d4ce
guxal <jonathanacp93@gmail.com>
2020-01-28 21:02:51 +0000

    hotfix url_prefix blueprint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,15 +4,17 @@ import models
 from models.city import City
 import json
 
-@app_views.route('/api/v1/status')
+
+@app_views.route('/status')
 def json_status():
     var = '{\n  "status": "OK"\n}\n'
     return var
 
-@app_views.route('/api/v1/stats')
+
+@app_views.route('/stats')
 def endpoint_stats():
-    amenities =  '{\n  "amenities": '+str(models.storage.count("Amenity"))+','
-    cities = '\n  "cities": '+str(models.storage.count("City"))+',' 
+    amenities = '{\n  "amenities": '+str(models.storage.count("Amenity"))+','
+    cities = '\n  "cities": '+str(models.storage.count("City"))+','
     places = '\n  "places": '+str(models.storage.count("Place"))+','
     reviews = '\n  "reviews": '+str(models.storage.count("Review"))+','
     states = '\n  "states": '+str(models.storage.count("State"))+','
