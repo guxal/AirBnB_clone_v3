e7fa9dd6a4cca9c0e47a94694d239245f78b91de
yenniferTobon <ymtobony@unal.edu.co>
2020-01-28 21:09:37 -0500

    __init__.py order of libraries

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from flask import Blueprint
-from api.v1.views.index import *
-import api.v1.views.states
-
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+import api.v1.views.states
