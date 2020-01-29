30612aaa8acbaa16c2dc743ebee153f5d00ff174
yenniferTobon <ymtobony@unal.edu.co>
2020-01-29 14:50:36 -0500

    index.py add comments and pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""start your API"""
+
 from api.v1.views import app_views
 import models
 from models.city import City
@@ -8,12 +10,14 @@ import json
 
 @app_views.route('/status')
 def json_status():
+    """will be to return the status"""
     var = {"status": "OK"}
     return jsonify(var)
 
 
 @app_views.route('/stats')
 def endpoint_stats():
+    """retrieves the number of each objects by type"""
     new_obj = {"amenities": models.storage.count("Amenity"),
                "cities": models.storage.count("City"),
                "places": models.storage.count("Place"),
@@ -22,9 +26,3 @@ def endpoint_stats():
                "users": models.storage.count("User")
                }
     return jsonify(new_obj)
-
-
-@app_views.route('/nop')
-def errors_404():
-    error = {'error': 'Not found'}
-    return jsonify(error)
