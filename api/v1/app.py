e0f5af88b1a880c2cc82b3ddf6b07332cc505ca2
yenniferTobon <ymtobony@unal.edu.co>
2020-01-28 21:05:32 -0500

    firts part of task 7 - all with pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-from flask import Flask 
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
-#app_views = Blueprint('app_views', __name__)
+
 
 @app.teardown_appcontext
 def close(Error):
