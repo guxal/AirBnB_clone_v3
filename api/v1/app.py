011bdb8f7d96de4ef0c3bbf2cb44e70e1d7aadf8
JonnyCash <jonathanacp93@gmail.com>
2020-01-29 12:47:43 -0500

    hotfix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-from flask import Flask 
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import environ
+
 
 app = Flask(__name__)
 
+host = environ.get('HBNB_API_HOST')
+port = environ.get('HBNB_API_PORT')
+
 app.register_blueprint(app_views)
+
 #app_views = Blueprint('app_views', __name__)
+
 
 @app.teardown_appcontext
 def close(Error):
     storage.close()
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+    app.run(host=host, port=port, threaded=True)
