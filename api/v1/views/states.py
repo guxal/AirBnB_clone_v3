00e587c2df42a291cea94e9ab662fece3972382e
yenniferTobon <ymtobony@unal.edu.co>
2020-01-30 14:34:48 -0500

    GET, GET/ID, DELETE task 7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,7 +2,7 @@
 """show all states or only state id"""
 from api.v1.views import app_views, index
 from models import storage, state
-from flask import jsonify, abort
+from flask import jsonify, abort, make_response
 
 
 @app_views.route('/states/', methods=['GET'], strict_slashes=False)
@@ -21,3 +21,16 @@ def retrieve_obj_id(state_id):
     if obj is None:
         abort(404)
     return jsonify(obj.to_dict())
+
+
+@app_views.route('states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_id(state_id):
+    """ funtion to deletes a State for id"""
+    obj = storage.get('State', state_id)
+    if obj is None:
+        abort(404)
+    else:
+        storage.delete(obj)
+        storage.save()
+    return make_response(jsonify({}), 200)
+
