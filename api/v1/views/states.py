125cb800630038ad91c8b87c0e4cbc549f83a044
guxal <jonathanacp93@gmail.com>
2020-01-30 20:21:39 +0000

    add post and update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """show all states or only state id"""
 from api.v1.views import app_views, index
-from models import storage, state
-from flask import jsonify, abort, make_response
+from models.state import State
+from models import storage
+from flask import jsonify, abort, make_response, request
 
 
 @app_views.route('/states/', methods=['GET'], strict_slashes=False)
@@ -34,3 +35,37 @@ def delete_id(state_id):
         storage.save()
     return make_response(jsonify({}), 200)
 
+
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    """Create new state"""
+    data = request.get_json(silent=True)
+    if data is None:
+        abort(404, "Not a JSON")
+    elif "name" not in data.keys():
+        abort(404, "Missing Name")
+    else:
+        new_state = State(**data)
+        storage.new(new_state)
+        storage.save()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id=None):
+    """Update state for id"""
+    data = storage.get('State', state_id)
+    if data is None:
+        abort(404)
+
+    r = request.get_json(silent=True)
+    if r is None:
+        abort(404, "Not a JSON")
+    else:
+        for key, value in r.items():
+            if key in ['id', 'created_at', 'updated_at']:
+                pass
+            else:
+                setattr(data, key, value)
+        storage.save()
+        return jsonify(data.to_dict()), 200
